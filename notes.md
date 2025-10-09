94b20c3b2439a197841b4809566a361858101e2e
Kernith <kernith@gmail.com>
2025-10-09 07:55:24 -0400

    Added an icon and added matplotlib backend stuff so PyInstaller doesn't miss it

---
+++ notes.md
@@ -0,0 +1,2 @@
+PyInstaller Command (run in venv):
+`pyinstaller --onefile --windowed --icon=images/icon.png --add-data "images;images" --add-data "config.json;." --add-data "pages;pages" --add-data "widgets;widgets" --add-data "utils;utils" main.py`
--- widgets/mpl_canvas.py
+++ widgets/mpl_canvas.py
@@ -1,6 +1,13 @@
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 
+# explicitly set the backend for PyInstaller
+matplotlib.use('Qt5Agg')
+
+# Force hidden imports for PyInstaller
+import _ctypes
+import pkg_resources.py2_warn
+
 class MplCanvas(FigureCanvas):
     def __init__(self):
         self.fig = Figure(figsize=(5, 4))
